7d5d581a2ce4547c64aed8c477f0204399189765
Alan Lima <dev.alanlima@gmail.com>
2023-05-17 16:20:35 -0300

    stylize

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,95 @@
 <script setup>
-import {RouterView} from 'vue-router'
+import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
     <header>
-        <NavBar/>
+        <NavBar />
     </header>
     <RouterView />
+
+    <div class="background">
+        <div class="circle1">
+            <div class="circle2">
+                <div class="circle1">
+                    <div class="circle2">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="circle3">
+            <div class="circle1"></div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.background {
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    border: 1px dotted aqua;
+    z-index: -1;
+}
+
+.circle1 {
+    width: 25rem;
+    height: 25rem;
+    background-color: var(--color-second);
+    border-radius: 50%;
+    animation: wiggle 45s infinite linear;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    -moz-filter: blur(15rem);
+    filter: blur(15rem);
+}
+
+.circle2 {
+    width: 35rem;
+    height: 35rem;
+    background-color: var(--color-third);
+    border-radius: 50%;
+    animation: wiggle 45s infinite linear;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    -moz-filter: blur(15rem);
+    filter: blur(15rem);
+}
+
+.circle3 {
+    width: 35rem;
+    height: 35rem;
+    background-color: var(--color-third);
+    border-radius: 50%;
+    animation: wiggle 60s infinite linear;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    -moz-filter: blur(15rem);
+    filter: blur(15rem);
+}
+
+@keyframes wiggle {
+    0% {
+        transform: rotate(0deg) translate(200%) rotate(360deg);
+    }
+
+    50% {
+        transform: rotate(-360deg) translate(200%) rotate(100deg);
+    }
+
+    100% {
+        transform: rotate(0deg) translate(200%) rotate(0deg);
+    }
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/generic/reset.css'
+import './styles/settings/colors.css'
+import './styles/settings/fonts.css'
+import './styles/elements/base.css'
 
 const app = createApp(App)
 
