7de82e27348317b582e8e86abe99cbef9211f68e
Alan Lima <dev.alanlima@gmail.com>
2023-05-17 19:41:48 -0300

    create / services folder

---
+++ src/services/Subtypes.js
@@ -0,0 +1,38 @@
+const subtypes = {
+    trainerSubtypes:[
+        "Goldenrod Game Corner", 
+        "Item",
+        "Pokémon Tool",
+        "Pokémon Tool F",
+        "Rapid Strike", 
+        "Rocket's Secret Machine",
+        "Single Strike",
+        "Stadium",
+        "Supporter",
+        "Technical Machine",
+    ],
+    pokemonSubtypes:[
+        "BREAK",
+        "Baby",
+        "Basic",
+        "EX",
+        "GX",
+        "LEGEND",
+        "Level-Up",
+        "MEGA",
+        "Restored",
+        "Stage 1",
+        "Stage 2",
+        "TAG TEAM",
+        "V",
+        "VMAX"
+    ],
+    energySubtypes:[
+        "Basic",
+        "Rapid Strike", 
+        "Single Strike",
+        "Special"
+    ]
+}
+
+export default subtypes
