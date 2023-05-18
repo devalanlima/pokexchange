e6449a0c4fb73c64880512c5037797c0f65f53cf
Alan Lima <dev.alanlima@gmail.com>
2023-05-18 14:05:16 -0300

    fix / filter does'nt render market, add renderKey

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -1,9 +1,49 @@
 <template>
-    <div>
-        
+    <div class="market-wrapper">
+        <div class="search-filter">
+        </div>
+
+        <div class="market-itens">
+            <SearchCards
+            :key="renderKey"
+            />
+        </div>
     </div>
 </template>
 
 <script setup>
-    
+import SearchCards from "@/components/SearchCards.vue"
+import { usePokemonFilters } from '../stores/pokemonFilters';
+const pokemonFilter = usePokemonFilters()
+import { watch, ref } from 'vue'
+const renderKey = ref(true)
+
+watch(pokemonFilter,()=>{
+    renderKey.value = !renderKey.value
+})
+
 </script>
+
+<style>
+.market-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 4.8fr;
+    gap: 2rem;
+}
+
+.search-filter {
+    max-width: 30rem;
+    min-width: 25rem;
+    max-height: 80vh;
+    border: 2px dotted red;
+    position: sticky;
+    top: 10rem;
+}
+
+.market-itens * {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    max-width: 150rem;
+}
+</style>
