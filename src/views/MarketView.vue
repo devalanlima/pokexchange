7fec24a7cd157af3b7745daf4d61861c4329a745
Alan Lima <dev.alanlima@gmail.com>
2023-05-20 19:22:37 -0300

    fix / renderKey now renders the component just one time

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -5,12 +5,13 @@
             <FilterSupertype />
             <FilterRarity />
             <FilterSubtype />
-            <FilterTypes 
-            v-if="pokemonFilter.supertype == 'Pokémon'"
-            />
+            <FilterTypes v-if="pokemonFilter.supertype == 'Pokémon'" />
+            <FilterHP v-if="pokemonFilter.supertype !== 'Energy'" :key="renderKey" />
         </div>
 
         <div class="market-itens">
+            <button @click="pokemonFilter.order = 'name'">A - Z</button>
+            <button @click="pokemonFilter.order = '-name'">Z - A</button>
             <SearchCards :key="renderKey" />
         </div>
     </div>
@@ -23,6 +24,7 @@ import FilterSupertype from "@/components/FilterSupertype.vue"
 import FilterRarity from "@/components/FilterRarity.vue"
 import FilterSubtype from "@/components/FilterSubtype.vue"
 import FilterTypes from "@/components/FilterTypes.vue"
+import FilterHP from "@/components/FilterHP.vue"
 
 import { watch, ref } from 'vue'
 import { usePokemonFilters } from '../stores/pokemonFilters';
@@ -31,23 +33,37 @@ const pokemonFilter = usePokemonFilters()
 
 const renderKey = ref(true)
 
-setTimeout(() => {
-    watch(pokemonFilter, () => {
-        renderKey.value = !renderKey.value
-    })
-}, 800);
+let timeoutId
 
-watch(()=> pokemonFilter.supertype, ()=>{
+watch(pokemonFilter, () => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+        renderKey.value = !renderKey.value;
+    }, 500);
+})
+
+watch(() => pokemonFilter.supertype, () => {
     pokemonFilter.rarity = '*'
     pokemonFilter.subtype = '*'
+    pokemonFilter.filterHP = ''
+    if (pokemonFilter.supertype == 'Pokémon') {
+        pokemonFilter.minHP = pokemonFilter.rangeMinHP = 30
+        pokemonFilter.maxHP = pokemonFilter.rangeMaxHP = 340
+    }
+    if (pokemonFilter.supertype == 'Trainer') {
+        pokemonFilter.minHP = pokemonFilter.rangeMinHP = 0
+        pokemonFilter.maxHP = pokemonFilter.rangeMaxHP = 70
+    }
 })
 </script>
 
-<style>
+<style scoped>
 .market-wrapper {
     display: grid;
     grid-template-columns: 1fr 4.8fr;
     gap: 2rem;
+    width: 100%;
+    position: relative;
 }
 
 .search-filter {
@@ -58,13 +74,21 @@ watch(()=> pokemonFilter.supertype, ()=>{
     top: 10rem;
     display: flex;
     flex-direction: column;
-    gap: 2rem;
+    gap: 3rem;
+}
+</style>
+
+<style>
+.market-itens {
+    border: 2px dotted red;
+    width: 100%;
 }
 
 .market-itens * {
     display: flex;
     flex-wrap: wrap;
-    gap: 2rem;
-    max-width: 150rem;
+    justify-content: space-around;
+    gap: .58rem 0;
+    margin: .58rem;
 }
 </style>
