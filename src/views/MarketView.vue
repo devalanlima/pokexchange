31385377eca248e4015a204613a5669b521de2b6
Alan Lima <dev.alanlima@gmail.com>
2023-05-20 12:58:38 -0300

    att / data

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -1,27 +1,46 @@
 <template>
     <div class="market-wrapper">
         <div class="search-filter">
+            <FilterName />
+            <FilterSupertype />
+            <FilterRarity />
+            <FilterSubtype />
+            <FilterTypes 
+            v-if="pokemonFilter.supertype == 'Pokémon'"
+            />
         </div>
 
         <div class="market-itens">
-            <SearchCards
-            :key="renderKey"
-            />
+            <SearchCards :key="renderKey" />
         </div>
     </div>
 </template>
 
 <script setup>
 import SearchCards from "@/components/SearchCards.vue"
-import { usePokemonFilters } from '../stores/pokemonFilters';
-const pokemonFilter = usePokemonFilters()
+import FilterName from "@/components/FilterName.vue"
+import FilterSupertype from "@/components/FilterSupertype.vue"
+import FilterRarity from "@/components/FilterRarity.vue"
+import FilterSubtype from "@/components/FilterSubtype.vue"
+import FilterTypes from "@/components/FilterTypes.vue"
+
 import { watch, ref } from 'vue'
+import { usePokemonFilters } from '../stores/pokemonFilters';
+
+const pokemonFilter = usePokemonFilters()
+
 const renderKey = ref(true)
 
-watch(pokemonFilter,()=>{
-    renderKey.value = !renderKey.value
-})
+setTimeout(() => {
+    watch(pokemonFilter, () => {
+        renderKey.value = !renderKey.value
+    })
+}, 800);
 
+watch(()=> pokemonFilter.supertype, ()=>{
+    pokemonFilter.rarity = '*'
+    pokemonFilter.subtype = '*'
+})
 </script>
 
 <style>
@@ -35,9 +54,11 @@ watch(pokemonFilter,()=>{
     max-width: 30rem;
     min-width: 25rem;
     max-height: 80vh;
-    border: 2px dotted red;
     position: sticky;
     top: 10rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
 }
 
 .market-itens * {
