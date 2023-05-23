ed51b07e954c7bf3f816cd189bee1ea56ece6bf9
Alan Lima <dev.alanlima@gmail.com>
2023-05-23 12:33:06 -0300

    add / OffSetPagination.vue component

--- src/views/MarketView.vue
+++ src/views/MarketView.vue
@@ -5,14 +5,23 @@
             <FilterSupertype />
             <FilterRarity />
             <FilterSubtype />
-            <FilterTypes v-if="pokemonFilter.supertype == 'Pokémon'" />
+            <FilterTypes v-if="pokemonFilter.supertype == 'Pokémon'" :key="renderKey" />
             <FilterHP v-if="pokemonFilter.supertype !== 'Energy'" :key="renderKey" />
         </div>
 
         <div class="market-itens">
-            <button @click="pokemonFilter.order = 'name'">A - Z</button>
-            <button @click="pokemonFilter.order = '-name'">Z - A</button>
+            <div class="functions-wrapper">
+                <div class="btn-wrapper">
+                    <button @click="pagination.pageSize = 25" >25</button>
+                    <button @click="pagination.pageSize = 50" >50</button>
+                    <button @click="pagination.pageSize = 100" >100</button>
+                    <button @click="pokemonFilter.order = 'name'">A - Z</button>
+                    <button @click="pokemonFilter.order = '-name'">Z - A</button>
+                </div>
+                <OffsetPagination/>
+            </div>
             <SearchCards :key="renderKey" />
+            <OffsetPagination/>
         </div>
     </div>
 </template>
@@ -25,17 +34,24 @@ import FilterRarity from "@/components/FilterRarity.vue"
 import FilterSubtype from "@/components/FilterSubtype.vue"
 import FilterTypes from "@/components/FilterTypes.vue"
 import FilterHP from "@/components/FilterHP.vue"
+import OffsetPagination from "@/components/OffsetPagination.vue"
 
 import { watch, ref } from 'vue'
-import { usePokemonFilters } from '../stores/pokemonFilters';
+import { usePokemonFilters } from '../stores/StorePokemonFilters';
+import { useOffsetPagination } from "../stores/StoreOffsetPagination"
 
 const pokemonFilter = usePokemonFilters()
+const pagination = useOffsetPagination()
 
 const renderKey = ref(true)
 
 let timeoutId
 
-watch(pokemonFilter, () => {
+watch([
+    pokemonFilter, 
+    ()=>{pagination.currentPage},
+    ()=>{pagination.pageSize}
+    ], () => {
     clearTimeout(timeoutId)
     timeoutId = setTimeout(() => {
         renderKey.value = !renderKey.value;
@@ -45,8 +61,11 @@ watch(pokemonFilter, () => {
 watch(() => pokemonFilter.supertype, () => {
     pokemonFilter.rarity = '*'
     pokemonFilter.subtype = '*'
+    pokemonFilter.type = []
+    pokemonFilter.selectedType = ''
     pokemonFilter.filterHP = ''
     pokemonFilter.isFilterHPOn = false
+    pagination.currentPage = 1
     if (pokemonFilter.supertype == 'Pokémon') {
         pokemonFilter.minHP = pokemonFilter.rangeMinHP = 30
         pokemonFilter.maxHP = pokemonFilter.rangeMaxHP = 340
@@ -56,6 +75,11 @@ watch(() => pokemonFilter.supertype, () => {
         pokemonFilter.maxHP = pokemonFilter.rangeMaxHP = 70
     }
 })
+
+watch(pokemonFilter, ()=>{
+    pagination.currentPage = 1
+})
+
 </script>
 
 <style scoped>
@@ -77,6 +101,17 @@ watch(() => pokemonFilter.supertype, () => {
     flex-direction: column;
     gap: 3rem;
 }
+
+.functions-wrapper{
+    display: flex;
+    position: relative;
+    align-items: center;
+}
+
+.btn-wrapper{
+    position: absolute;
+    right: 0;
+}
 </style>
 
 <style>
@@ -92,4 +127,5 @@ watch(() => pokemonFilter.supertype, () => {
     gap: .58rem 0;
     margin: .58rem;
 }
+
 </style>
