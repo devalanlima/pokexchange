809398e575de213e63630e52ca280dce0de11d52
Alan Lima <dev.alanlima@gmail.com>
2023-05-23 17:34:44 -0300

    add / three cards

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,9 +1,18 @@
 <template>
     <article>
-        <h2>Buy, Sell and Trade hundreds NFT <span class="pokemonString">Pokémon</span> <span
-                class="cardsString">Cards</span></h2>
-        <p class="info">A non-fungible token (NFT) is a unique digital identifier that cannot be copied, substituted, or
-            subdivided, that is recorded in a blockchain, and that is used to certify ownership and authenticity.</p>
+        <h2>
+            Buy, Sell and Trade hundreds NFT 
+            <span class="pokemonString">
+                Pokémon
+            </span> 
+            <span class="cardsString">
+                Cards
+            </span>
+        </h2>
+        <p class="info">
+            A non-fungible token (NFT) is a unique digital identifier that cannot be copied, substituted, or
+            subdivided, that is recorded in a blockchain, and that is used to certify ownership and authenticity.
+        </p>
         <div class="datas">
             <div class="data">
                 <p>950M</p>
@@ -18,11 +27,49 @@
                 <p>Daily Trades</p>
             </div>
         </div>
+        <div class="three-cards">
+            <SearchCards
+                :order="'number'"
+                :pageSize="1"
+                :currentPage="2"
+                :name="'char'"
+                :subtype="'MEGA'"
+                :supertype="'*'"
+                :rarity="'*'"
+                :type="''"
+                :hp="''" 
+                :imageSize="'large'"
+            />
+            <SearchCards
+                :order="'number'"
+                :pageSize="1"
+                :currentPage="1"
+                :name="'blastoise'"
+                :subtype="'MEGA'"
+                :supertype="'*'"
+                :rarity="'*'"
+                :type="''"
+                :hp="''" 
+                :imageSize="'large'"
+            />
+            <SearchCards
+                :order="'number'"
+                :pageSize="1"
+                :currentPage="1"
+                :name="'ven'"
+                :subtype="'MEGA'"
+                :supertype="'*'"
+                :rarity="'*'"
+                :type="''"
+                :hp="''" 
+                :imageSize="'large'"
+            />
+        </div>
     </article>
 </template>
 
 <script setup>
-
+import SearchCards from './SearchCards.vue';
 </script>
 
 <style scoped>
@@ -35,14 +82,14 @@ article {
     flex-wrap: wrap;
     position: relative;
     gap: 4rem;
-    padding: 0 2rem;
+    padding: 0 10rem;
 }
 
 h2 {
     font-family: var(--font-Noto-Sans);
     font-weight: 700;
     font-size: 6rem;
-    width: 70rem;
+    width: 60rem;
     line-height: 8rem;
 }
 
@@ -55,7 +102,7 @@ h2 {
 }
 
 .info {
-    width: 70rem;
+    width: 60rem;
     font-family: var(--font-Roboto);
     font-weight: 300;
     text-align: justify;
@@ -64,7 +111,7 @@ h2 {
 }
 
 .datas {
-    width: 70rem;
+    width: 60rem;
     display: flex;
     justify-content: space-between;
     justify-items: start;
@@ -85,4 +132,48 @@ h2 {
 .data :last-child {
     font-size: 2.5rem;
     font-weight: 200;
-}</style>
+}
+
+.three-cards{
+    box-sizing: border-box;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    right: 0;
+    width: 69rem;
+    height: 50rem;
+    
+}
+
+</style>
+
+<style>
+.three-cards .loading{
+    height: 43rem;
+    width: 30rem;
+}
+.three-cards img{
+    height: 43rem;
+    width: 30rem;
+}
+.three-cards :nth-last-child(3){
+    position: absolute;
+    left: 0;
+    top: 2rem;
+    z-index: 2;
+    rotate: -13deg;
+}
+.three-cards :nth-child(2){
+    position: absolute;
+    left: 12rem;
+    top: 0;
+    z-index: 1;
+}
+.three-cards :nth-child(3){
+    position: absolute;
+    left: 24rem;
+    rotate: 13deg;
+    top: 2rem;
+}
+</style>
