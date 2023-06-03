ae79db2df70a061ba3fdddea812e86d96d097c44
Alan Lima <dev.alanlima@gmail.com>
2023-06-03 12:20:14 -0300

    add / text to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,28 +1,41 @@
 <template>
-    <div class="wrapper">
-        <ParallaxEffect>
-            <img src="https://images.pokemontcg.io/xy8/40.png" alt="">
-        </ParallaxEffect>
-    </div>
-    <div class="wrapper">
-        <ParallaxEffect>
-            <img src="https://images.pokemontcg.io/xy8/40.png" alt="">
-        </ParallaxEffect>
-    </div>
-    <div class="wrapper">
-        <ParallaxEffect>
-            <img src="https://images.pokemontcg.io/xy8/40.png" alt="">
-        </ParallaxEffect>
-    </div>
-
+    <article>
+        <p>
+            Our featured NFTs on this website are artistic representations and not real NFTs. They serve as conceptual
+            pieces, showcasing what NFTs could potentially be. Please note that these simulated NFTs cannot be purchased or
+            traded as authentic NFTs.
+        </p>
+        <p>
+            NFTs, or non-fungible tokens, are a type of digital asset that represent ownership or proof of authenticity of a
+            unique item or piece of content. Unlike cryptocurrencies such as Bitcoin or Ethereum, which are fungible and can
+            be exchanged on a one-to-one basis, NFTs are indivisible and cannot be exchanged on a like-for-like basis.
+        </p>
+        <p>
+            NFTs are built using blockchain technology, typically on Ethereum's blockchain, which ensures their immutability
+            and traceability. Each NFT has a unique identifier that distinguishes it from other tokens, making it
+            one-of-a-kind. This uniqueness makes NFTs ideal for representing digital art, collectibles, virtual real estate,
+        </p>
+        <p>
+            One of the key features of NFTs is their ability to prove ownership and establish provenance. The blockchain
+            provides a transparent and decentralized ledger that records all transactions and ownership changes,
+            ensuring the authenticity and history of the NFT. This has opened up new possibilities for artists,
+            creators, and collectors to monetize and trade digital assets in a secure and verifiable manner.
+        </p>
+    </article>
 </template>
 
-<script setup>
-import ParallaxEffect from '../components/ParallaxEffect.vue';
-
-
-</script>
-
 <style scoped>
-
+article{
+    min-height: 80vh;
+    max-width: 100rem;
+    margin: 3rem auto 6rem;
+}
+p {
+    font-size: 2.5rem;
+    padding: 1rem 3rem;
+    line-height: 3rem;
+    text-indent: 6rem;
+    text-align: justify;
+}
 </style>
+
